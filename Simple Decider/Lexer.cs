9515db67db0c067f8ea64944825835b0fecb76c8
Ahmed Ghannam <amalghannam@crimson.ua.edu>
2016-12-28 19:47:04 -0800

    Add project files.

---
+++ Simple Decider/Lexer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decider
+{
+    public enum Token
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide,
+        Open,
+        Close,
+        Num,
+        Var,
+        Eof
+    };
+
+    class Lexer
+    {
+
+        private List<Token> tokens;
+        private int step = 0;
+
+        public Lexer()
+        {
+
+        }
+
+        /// <summary>
+        /// Advances the character reading the source string by one position. 
+        /// </summary>
+        private void Advance()
+        {
+            step++;
+        }
+
+        /// <summary>
+        /// Tokenizes and returns a list of tokens identified in the given expression. 
+        /// </summary>
+        /// <param name="expression">the source expression to tokenize</param> 
+        /// <returns>the token stream of the given expression</returns> 
+        public List<Token> Tokenize(string expression)
+        {
+            if (expression.Length == 0) return null;
+
+            tokens = new List<Token>();
+
+            while (step < expression.Length)
+            {
+                switch (expression[step])
+                {
+                    case ' ':
+                        Advance();
+                        continue;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        tokens.Add(Token.Num);
+                        Advance();
+                        break;
+                    case 'a':
+                    case 'b':
+                    case 'c':
+                    case 'd':
+                    case 'e':
+                    case 'f':
+                    case 'g':
+                    case 'h':
+                    case 'i':
+                    case 'j':
+                    case 'l':
+                    case 'm':
+                    case 'n':
+                    case 'o':
+                    case 'q':
+                    case 'r':
+                    case 't':
+                    case 'u':
+                    case 'v':
+                    case 'w':
+                    case 'x':
+                    case 'y':
+                    case 'z':
+                        tokens.Add(Token.Var);
+                        Advance();
+                        break;
+                    case '+':
+                        tokens.Add(Token.Plus);
+                        Advance();
+                        break;
+                    case '-':
+                        tokens.Add(Token.Minus);
+                        Advance();
+                        break;
+                    case '*':
+                        tokens.Add(Token.Multiply);
+                        Advance();
+                        break;
+                    case '/':
+                        tokens.Add(Token.Divide);
+                        Advance();
+                        break;
+                    case '(':
+                        tokens.Add(Token.Open);
+                        Advance();
+                        break;
+                    case ')':
+                        tokens.Add(Token.Close);
+                        Advance();
+                        break;
+                    default:
+                        Console.WriteLine("No.");
+                        Environment.Exit(1);
+                        break; 
+                }
+            }
+            tokens.Add(Token.Eof); 
+            return tokens;
+        }
+    }
+}
---
+++ Simple Decider/Parser.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Decider
+{
+    class Parser
+    {
+        private Token lookahead;
+        private List<Token> tokens;
+
+        public Parser()
+        {
+            tokens = new List<Token>();
+        }
+
+        public void Parse(List<Token> input)
+        {
+            tokens.AddRange(input);
+            lookahead = tokens[0];
+
+            Expression();
+
+            if (!Predict(Token.Eof))
+            {
+                Console.WriteLine("No."); 
+            } else
+            {
+                Console.WriteLine("Yes."); 
+            }
+        }
+
+        // E  -> T E'
+        private void Expression()
+        {
+            Term();
+            ExpressionPrime(); 
+        }
+
+        // E' -> + T E' | -TE' |epsilon
+        private void ExpressionPrime()
+        {
+            if (Predict(Token.Plus))
+            {
+                Match(Token.Plus);
+                Term();
+                ExpressionPrime();
+            } else if (Predict(Token.Minus))
+            {
+                Match(Token.Minus);
+                Term();
+                ExpressionPrime();
+            } 
+        }
+
+        // T  -> F T'
+        private void Term()
+        {
+            Factor();
+            TermPrime(); 
+        }
+
+        // T' -> * F T' | /FT' |epsilon
+        private void TermPrime()
+        {
+            if (Predict(Token.Multiply))
+            {
+                Match(Token.Multiply);
+                Factor();
+                TermPrime(); 
+            } else if (Predict(Token.Divide))
+            {
+                Match(Token.Divide);
+                Factor();
+                TermPrime(); 
+            }
+        }
+
+        // F  -> (E) | int | var
+        private void Factor()
+        {
+            if (Predict(Token.Open))
+            {
+                Match(Token.Open);
+                Expression();
+                Match(Token.Close);
+            } else if (Predict(Token.Num))
+            {
+                Match(Token.Num);
+            } else if (Predict(Token.Var)) {
+                Match(Token.Var); 
+            } else
+            {
+                Error(); 
+            }
+        }
+
+
+        /*
+         * Helper methods start here */ 
+          
+        private void Next()
+        {
+            tokens.RemoveAt(0);
+            lookahead = tokens[0];
+        }
+
+        private bool Predict(Token token)
+        {
+            return lookahead == token;
+        }
+
+        private void Match(Token token)
+        {
+            Verify(token);
+            Next();
+        }
+
+        private void Error()
+        {
+            Console.WriteLine("No.");
+            Environment.Exit(1);
+        }
+
+        private void Verify(Token token)
+        {
+            if (!Predict(token))
+            {
+                Error(); 
+            }
+        }
+    }
+}
